3353ce3bcb3025495fe55f0dde641ef88c760f9b
uuuu4ufo <uuuu4ufo@hotmail.com>
2021-05-16 15:25:43 +0800

    version 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,21 +1,18 @@
 package servlet_examples;
 
-import java.io.*;	//拿掉地10, 15行報錯
-import javax.servlet.*;		//拿掉第10行報錯
-import javax.servlet.http.*;	//拿掉則7, 9行報錯
+import java.io.*;	
+import javax.servlet.*;		
+import javax.servlet.http.*;	
 
-public class HelloWorld extends HttpServlet {	//就算不繼承也不會出現編譯錯誤, 因為doGet不是抽象方法
+public class HelloWorld extends HttpServlet {	
 
-  public void doPost(HttpServletRequest req, HttpServletResponse res)	//override條件1.方法更公開 2.例外只能更低階或沒有
+  public void doPost(HttpServletRequest req, HttpServletResponse res)	
                                throws ServletException, IOException {
-	  
-	  //用doPost應該要不能執行(但我可以??)
 
-    res.setContentType("text/html; charset=Big5");	//編碼沒有寫就是預設ISO-8859-1(西歐語系)
-//  上列(簡易版)等同於=> (標準版)res.setHeader("Content-Type", "text/html; charset=BIG5"); => ContentType太常用
-//  可對照form.html的HEAD標籤, 內容是一樣的
+
+    res.setContentType("text/html; charset=Big5");	
     
-    PrintWriter out = res.getWriter();	//不能用PrintStream(非西歐語系會亂碼~)
+    PrintWriter out = res.getWriter();
 
     out.println("<HTML>");
     out.println("<HEAD><TITLE>Hello World</TITLE></HEAD>");
@@ -23,9 +20,7 @@ public class HelloWorld extends HttpServlet {	//就算不繼承也不會出現編譯錯誤, 因
     out.println("<BIG>Hello World , 世界你好 !</BIG>");
     out.println("</BODY></HTML>");
 
-    //* 課堂補充
-    out.println(getServletContext().getAttribute(ServletContext.TEMPDIR));
-    // C:\TFA101_WebApp\eclipse_WTP_workspace1\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\work\Catalina\localhost\SL314
+    
   }
   
   public void doGet(HttpServletRequest req, HttpServletResponse res) 
